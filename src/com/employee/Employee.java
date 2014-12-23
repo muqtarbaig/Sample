d1670e97829fb51e4ccf698eae72e80bc7b18202
muqtarbaig <muqtarbaig@gmail.com>
2014-12-23 19:47:12 +0530

    Update Employee.java

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -1,13 +1,28 @@
 package com.employee;
 
-public class Employee {
+import java.util.Map;
 
-	int id;
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.SessionAware;
+
+public class Employee implements SessionAware {
+
+	String id;
 	String name;
-	public int getId() {
+	SessionMap<String, Object> sessionMap ;
+	
+	String salary;
+	
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 	public String getName() {
@@ -15,5 +30,30 @@ public class Employee {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public String execute(){
+		System.out.println("Employee object");
+		System.out.println(this.getName());
+	
+		try{
+		if(this.getId().equals(this.getName())){
+			sessionMap.put("login", "true");
+		this.setSalary("2000");
+		return "success";
+		}
+		}catch(Exception ex){
+			return "invalid";
+		}
+		return "failure";
+	}
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		sessionMap = (SessionMap<String, Object>) arg0;
+	}
+	
+	public String logout(){
+		sessionMap.invalidate();
+		return "success";
 	}
 }
